0f29cb717d8e537dd85a6f8fac756d0acf5352d4
shinohara-san <someiyoshino0809@gmail.com>
2021-06-18 22:42:40 +0900

    Initial Commit

---
+++ GsSwiftSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GsSwiftSample
+//
+//  Created by Yuki Shinohara on 2021/06/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
